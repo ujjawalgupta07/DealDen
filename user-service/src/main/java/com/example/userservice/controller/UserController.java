aefc4369bc74de9da4aac8f7e9d18babacec6e08
ujjagupta <ug042819@broadcom.com>
2025-04-22 12:03:09 +0530

    Api Gateway setup

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "User Service is Working";
+    }
+}
